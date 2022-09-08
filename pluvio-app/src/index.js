4c091c90de8dc25807fe1af684246dbb79f34a01
Feizza Fazilatun <feizza01@gmail.com>
2022-09-08 14:36:45 -0400

    fixed side navigation bar

--- pluvio-app/src/index.js
+++ pluvio-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NavigationBar from './components/NavigationBarComponent/NavigationBar';
-
+import "@fontsource/poppins";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
