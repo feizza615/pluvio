68e6e7bcd9dfa5bb12bea842c672f9633c0c8390
Justin Too <justinktoo98@gmail.com>
2022-09-11 17:27:51 -0400

    Setup welcomepage

--- pluvio-app/src/mainPages/WelcomePage/WelcomePage.js
+++ pluvio-app/src/mainPages/WelcomePage/WelcomePage.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import Header from "../../components/HeaderComponent/Header";
+import NavigationBar from "../../components/NavigationBarComponent/NavigationBar";
 
 export default class WelcomePage extends Component {
   render() {
     return (
-      <div>WelcomePage</div>
+      <div id="container">
+        <div id="header">
+          <Header />
+        </div>
+
+        <NavigationBar />
+      </div>
+      
+      
     )
   }
 }
