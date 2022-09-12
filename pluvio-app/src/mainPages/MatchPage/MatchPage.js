b0277e55badfbfa3d7275edaecfa9b65b409ddf6
Feizza Fazilatun <feizza01@gmail.com>
2022-09-12 17:23:54 -0400

    attempt to hide key

--- pluvio-app/src/mainPages/MatchPage/MatchPage.js
+++ pluvio-app/src/mainPages/MatchPage/MatchPage.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import axios from "axios";
+import React from "react";
+import {config} from "../../config"
 
-export default class MatchPage extends Component {
-  render() {
-    return (
-      <div>MatchPage</div>
-    )
-  }
+const baseURL = "https://api.themoviedb.org/3/movie/550?api_key="+config.DB_KEY;
+
+export default function MatchPage() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  const myJSON = JSON.stringify(post);
+  if (!post) return null;
+
+  return (
+    <div>
+      <h1>{post.homepage}</h1>
+    </div>
+  );
 }
