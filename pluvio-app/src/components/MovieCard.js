aab1ad7f0ab6b9dd9056c1c536815c186273993f
PreetomCho <preetom56@gmail.com>
2022-09-19 23:22:17 -0400

    Setup Movie Card Description. NEED: insert database, fix up design, shows up in homepage as well for some reason

--- pluvio-app/src/components/MovieCard.js
+++ pluvio-app/src/components/MovieCard.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import Card from './Card'
+import styled from "styled-components";
 
-const MovieCard = () => {
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  h4 {
+    width: 50%;
+    box-sizing: border-box;
+    text-align: left;
+    margin: 5px 0;
+  }
+`;
+
+export const RowContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  position: relative;
+  overflow: hidden;
+  isolation: isolate;
+`;
+
+export const Image = styled.div` 
+  width: 300px;
+  height: 200px;
+  background: red;
+  border-radius: 15px;
+`;
+
+const MovieCard = ({
+  title,
+  release,
+  platform,
+  score,
+  rating,
+  genre,
+  duration,
+  description,
+}) => {
   return (
-    <>
-        <h1>Test</h1>
-    </>
+    <Card>
+      <RowContainer>
+        <Image />
+        <div className="container">
+          <h2 style={{marginBottom: "5px", textAlign: "left" }}>{title}</h2>
+          <Container>
+            <h4>Release: {release} </h4>
+            <h4>Platform: {platform} </h4>
+            <h4>Score: {score} </h4>
+            <h4>Rating: {rating} </h4>
+            <h4>Genre: {genre} </h4>
+            <h4>Duration: {duration} </h4>
+          </Container>
+        </div>
+      </RowContainer>
+      <p style={{textAlign: "left", fontSize: "20px", padding: "20px"}}>{description}</p>
+    </Card>
   )
 }
 
--- pluvio-app/src/mainPages/FriendsPage/FriendsPage.js
+++ pluvio-app/src/mainPages/FriendsPage/FriendsPage.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
+import MovieCard from '../../components/MovieCard';
+import Card from '../../components/Card';
+import "styled-components";
+
+//Testing Sample Data for MovieCard
+const data = {
+  title: "Despicable Me 3",
+  release: "June 30, 2017",
+  platform: "Netflix, Hulu",
+  score: "8/10",
+  rating: "PG-13",
+  genre: "Comedy, Children",
+  duration: "2 Hours",
+  description: "blah blah blah"
+};
 
 export default class FriendsPage extends Component {
   render() {
     return (
-      <h1>FriendsPage</h1>
+      //<h1>FriendsPage</h1>
+      //Testing MovieCard.js Here
+      <div className="App">
+      <MovieCard
+        title={data.title}
+        release={data.release}
+        platform={data.platform}
+        score={data.score}
+        rating={data.rating}
+        genre={data.genre}
+        duration={data.duration}
+        decsription={data.description}
+      />
+    </div>
     )
   }
 }
