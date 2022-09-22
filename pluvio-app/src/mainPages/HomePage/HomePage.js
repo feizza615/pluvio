fab12b16c2ed492a60112677817ac79a26f4e025
Azarul <theazarul100@gmail.com>
2022-09-21 21:32:17 -0400

    Profile Box Completed

--- pluvio-app/src/mainPages/HomePage/HomePage.js
+++ pluvio-app/src/mainPages/HomePage/HomePage.js
@@ -6,6 +6,8 @@ import NavigationBar from "../../components/NavigationBarComponent/NavigationBar
 import Card from "../../components/Card";
 import "./HomePage.css";
 import styled from "styled-components";
+import Picture from "./ProfilePic.png";
+
 
 
 export const Text = styled.p`
@@ -19,17 +21,34 @@ export const Text = styled.p`
 export default class HomePage extends Component {
   render() {
     return (
-      <div id="container">
-        <div id="header">
-          <Header/>
+      <>
+        <div id="container">
+          <div id="header">
+            <Header />
+          </div>
+          <NavigationBar />
+          <Container>
+            <MovieCard />
+          </Container>
         </div>
-        <NavigationBar />
-        <Container>
-          <MovieCard />
-        </Container>
-      </div>
+        <Card>
+          <div style={{ marginTop: "-1em" }} className="top">
+            <img src={Picture} alt="profile" />
+            <div className="texts">
+              <Text style={{ fontSize: "25px", marginBottom: "-0.5em" }}>@Username</Text>
+              <Text style={{ color: "gray", fontSize: "17px", }}>Edit Profile</Text>
+            </div>
+          </div>
+          <div className="bottom">
+            <Text> Following </Text>
+            <Text> Follower </Text>
+            <Text> Review </Text>
+          </div>
+        </Card>
+      </>
     );
   }
 }
+
 
 
