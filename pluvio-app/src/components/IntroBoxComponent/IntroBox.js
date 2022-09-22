7416beeef87568350bb7205e9fdebe5c873dac67
mazarul <mazarulmohammed2001@gmail.com>
2022-09-22 13:08:38 -0400

    made intro box

---
+++ pluvio-app/src/components/IntroBoxComponent/IntroBox.js
@@ -0,0 +1,31 @@
+import ButtonComponent from "../LoginComponent/ButtonComponent";
+import styled from "styled-components";
+import balls from "./balls.png"
+
+export const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins";
+  font-weight: bolder;
+`;
+
+const IntroBox = () => {
+  return (
+    <>
+      <Card>
+        <Title>Find your perfect movie</Title>
+        <p style={{fontWeight: 900}}>
+          We’re constantly updating your recommendations based on your responses
+          and watchlist. Look for what you’re most interested in and we can find
+          the best movie for you!
+        </p>
+        <ButtonComponent>Match Me</ButtonComponent>
+        <img src={balls} alt="" 
+        style={{position: "absolute", bottom: "-5vh", right: "20px"}}
+/>
+      </Card>
+    </>
+  );
+};
+
+export default IntroBox;
