b1b3dd9840a21d1c9fe3ab52c93362730e390ab8
Mihad Khondker <mihadkho2019@gmail.com>
2022-09-25 22:32:51 -0400

    added searching functionality

--- pluvio-app/src/components/ReviewBoxComponent/ReviewBox.js
+++ pluvio-app/src/components/ReviewBoxComponent/ReviewBox.js
@@ -4,13 +4,20 @@ import React from 'react';
 import Card from "../Card";
 import ButtonComponent from "../LoginComponent/ButtonComponent";
 import MoviePicture from "./MoviePic.png";
+import Picture from "./ProfilePic.png";
 
 export default function ReviewBox({reviewdata}) {
     return (
         <Card>
             <div className="view">
-                <div className="Container">
-                    <p className = "topLine" style={{ fontSize: "25px", }}>@{reviewdata ? reviewdata.username : "username"}<RatingArea/></p>
+                <div>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                        <img src={Picture} alt="profile" />
+                        <div style={{}}>
+                            <p className = "topLine" style={{ fontSize: "25px", margin: 0}}>@{reviewdata ? reviewdata.username : "username"}</p>
+                            <RatingArea/>
+                        </div>
+                    </div>
                     <p className = "bottomLine">{reviewdata ? reviewdata.description : "I really enjoyed this movie, I believe many others should try this out if they like this genre. I really enjoyed this movie, I believe many others should try this out if they like this genre. I really enjoyed this movie, I believe many others should try this out if they like this genre."} </p>
                 </div>
                 <div className="side">
