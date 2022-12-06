bb8f967ad036df43d7d607c0f8f1f05df4a8ad6b
justin <justinktoo98@gmail.com>
2022-12-05 19:19:44 -0500

    Recommendations are more random now, and wont get stuck in a loop

--- pluvio-app/src/mainPages/MatchPage/MatchPage.js
+++ pluvio-app/src/mainPages/MatchPage/MatchPage.js
@@ -17,17 +17,23 @@ import {movieID} from "./movieid"
 const notebookURL = "/convert?index=";
 const baseURL =
   "https://api.themoviedb.org/3/search/movie?api_key=" + config.DB_KEY;
+  const baseURL2 = "https://api.themoviedb.org/3/movie/popular?api_key=" + config.DB_KEY + "&language=en-US&page=1"
+
 export default function MatchPage() {
-  const [isLoaded, setIsLoaded] = useState(true);
+  let randomIndex = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
+  const [isLoaded, setIsLoaded] = useState(false);
   const [directionTinder, setDirection] = useState("swipeCornerDownRight");
   const [isOpen, setOpen] = useState(false);
   const [reccList, setReccList] = useState([]);
-  const [id, setId] = useState(118);
+  // const [id, setId] = useState(118);
+  const [id, setId] = useState(movieID[randomIndex]);
   const [like, setLike] = useState(null);
   const [dislike, setMap] = useState([]);
+  const [handleDupe, setDupe] = useState(false)
+ 
   const [details, setDetails] = useState([
     {
-      title: "Despicable Me 3",
+      title: id,
       release: "June 30, 2017",
       score: 8,
       genre: [{ name: "Genre" }],
@@ -35,35 +41,75 @@ export default function MatchPage() {
       description:"DespicableDespicable Me 3 is a 2017 American computer-animated comedy film produced by Illumination and distributed by Universal Pictures. It is the sequal to Despicable Me 2, the third main installment, and the fourth installment overall in the Despicable Me.",
     },
   ]);
+  
+
   useEffect(() => {
-    console.log("PREVIOUS MOVIE WAS " + reccList[0])
+    const fetchPopular = async() => {
+
+    const data = await fetch(baseURL2);
+  const movies = await data.json();
+  console.log("MOVIES: " + movies.results[0].id)
+  console.log(movies.results[0])
+    setId(movies.results[0].id)
+    // setDetails({
+    //   title: "hi",
+    //   release: "June 30, 2017",
+    //   score: 8,
+    //   genre: [{ name: "Genre" }],
+    //   duration: "120",
+    //   description:"DespicableDespicable Me 3 is a 2017 American computer-animated comedy film produced by Illumination and distributed by Universal Pictures. It is the sequal to Despicable Me 2, the third main installment, and the fourth installment overall in the Despicable Me.",
+   
+    // })
+    // setStatus(true)
+    // setIsLoaded(true)
+  }
+    fetchPopular();
+
+    // console.log("DIPLICATE?? " + handleDupe)
+    // console.log("PREVIOUS MOVIE WAS " + reccList[0])
     let det = [];
-    let dislikeArr = [];
-    dislikeArr.push(dislike)
-    console.log("DISLIKE ARR: " + dislikeArr)
-    setIsLoaded(false);
+    // if(isLoaded === false){
+    //   fetchPopular()
+    // }
+    if(handleDupe){
+      // console.log("DUPED")
+      let temp = reccList;
+      temp.shift();
+      setReccList(temp)
+    }
     if (like == null) {
+      console.log("using ID: " + id)
       fetch(notebookURL + id)
         .then((res) => res.json())
         .then(
           (result) => {
             setReccList(result); //[23,24,25,26]
+            console.log("Similiar movies: " + result)
           },
           (error) => {
-            console.log(error);
+            // console.log(error);
           }
         );
-      console.log(reccList);
+        setIsLoaded(true)
+      // console.log(reccList);
     }
     //Dislike: Continue showing movies from reccList
  
-    else if(reccList.length>2){
+     if(reccList.length>2){
+      
       //If user dislikes a movie add it to the map
-      if(like == false){
-        console.log("I dislike " + reccList[0]);
+      if(like === false){
         
+        // if(reccList.length === 4){
+        //   setDupe(true)
+        // }
+        // console.log("I dislike " + reccList[0]);
+        // console.log("Next movie up is: " + reccList[1])
+        if(dislike.length >= 30){
+          setMap([])
+        }
         if(dislike.includes(reccList[1])){
-          console.log("WE DETECTED DISLIKE")
+          console.log("WE DETECTED DISLIKE... Skipping " + reccList[1])
           let temp = reccList;
           temp.shift();
           setReccList(temp)
@@ -71,34 +117,36 @@ export default function MatchPage() {
           dislike.push(reccList[0])
         }
         
-        console.log([...dislike]);
+        // console.log([...dislike]);
       let temp = reccList;
 
         temp.shift();
         setReccList(temp);
-        console.log(reccList);
+        // console.log(reccList);
       }
 
       //delete current movie
 
 
-      if(like == true){
+      if(like === true){
+        
+        // if(reccList.length === 4){
+        //   setDupe(true)
+        // }
+        let temp;
         fetch(notebookURL + reccList[0])
         .then((res) => res.json())
         .then(
-          (result) => {
-            let temp = result;
-            temp.shift();
-            setReccList(temp);
+          (result) => {            
+            // temp = result;
+            setReccList(result);
           },
           (error) => {
-            console.log(error);
-          }
+            // console.log(error);
+          },
           
         );
-        
-
-      console.log(reccList);
+      // console.log(reccList);
       }
 
 
@@ -120,16 +168,16 @@ export default function MatchPage() {
     else{
       let arr=[]
       if(dislike.includes(reccList[1])){
-        console.log("WE DETECTED DISLIKE BUT HAVE NO SPACE")
-        console.log("DISLIKE**")
-        console.log(dislike)
+        // console.log("WE DETECTED DISLIKE BUT HAVE NO SPACE")
+        // console.log("DISLIKE**")
+        // console.log(dislike)
       // for(let i = 0; i < 10; i++){
         let num = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
         setReccList(num);
-        let num2 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
-        setReccList(num2);
-        let num3 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
-        setReccList(num3);
+        // let num2 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
+        // setReccList(num2);
+        // let num3 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
+        // setReccList(num3);
         // arr.push(num)
       // }
       // let temp = arr;
@@ -137,7 +185,7 @@ export default function MatchPage() {
       // let temp2 = reccList;
       // temp2.shift();
       // setReccList(temp2)
-      console.log("Fetching Notebook: " + notebookURL + "\nnum: " + num)
+      // console.log("Fetching Notebook1: " + notebookURL + "\nnum: " + num)
       fetch(notebookURL + movieID[num])
         .then((res) => res.json())
         .then(
@@ -145,21 +193,18 @@ export default function MatchPage() {
             setReccList(result);
           },
           (error) => {
-            console.log(error);
+            // console.log(error);
           }
         );
-        // let temp = reccList;
-        // temp.shift();
-        // setReccList(temp)
       } else {
         dislike.push(reccList[0])
         // for(let i = 0; i < 10; i++){
           let num = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
           setReccList(num);
-          let num2 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
-          setReccList(num2);
-          let num3 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
-          setReccList(num3);
+          // let num2 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
+          // setReccList(num2);
+          // let num3 = Math.floor(Math.random() * (4803 - 1 + 1)) + 1;
+          // setReccList(num3);
           // arr.push(num)
         // }
         // let temp = arr;
@@ -167,24 +212,26 @@ export default function MatchPage() {
         // let temp2 = reccList;
         // temp2.shift();
         // setReccList(temp2)
-        console.log("Fetching Notebook: " + notebookURL + "\nnum: " + num)
+        // console.log("Fetching Notebook2: " + notebookURL + "\nnum: " + num)
+        
         fetch(notebookURL + movieID[num])
           .then((res) => res.json())
           .then(
             (result) => {
               setReccList(result);
+              
             },
             (error) => {
-              console.log(error);
+              // console.log(error);
             }
           );
       }
 
       
-      console.log(reccList);
+      // console.log(reccList);
     }
     async function test() {
-      console.log("CURRNT MOVIE"+reccList[0]);
+      // console.log("CURRNT MOVIE"+reccList[0]);
       axios
         .get(
           "https://api.themoviedb.org/3/movie/" +
@@ -193,9 +240,9 @@ export default function MatchPage() {
             config.DB_KEY
         )
         .then((response) => {
-          console.log(JSON.parse(JSON.stringify(response.data)));
+          // console.log(JSON.parse(JSON.stringify(response.data)));
           det.push(JSON.parse(JSON.stringify(response.data)));
-          console.log(det);
+          // console.log(det);
           if (det.length > 0) {
             setDetails(det);
           } else {
@@ -213,20 +260,20 @@ export default function MatchPage() {
           }
         });
     }
-    test().then(console.log("DATA SET"), setIsLoaded(true));
+    test().then( );
   }, [id]); // <- add the count variable here
   var Tinder = null;
   const onTinderSwipeRight = () => {
     setId((id) => id + 1);
     setLike(false);
-    console.log("Dislike");
+    // console.log("Dislike");
     setDirection("swipeCornerDownRight");
     Tinder.swipe();
   };
   const onTinderSwipeLeft = () => {
     setLike(true);
     setId((id) => id - 1);
-    console.log("Like");
+    // console.log("Like");
     setDirection("swipeCornerDownLeft");
     Tinder.swipe();
   };
@@ -245,17 +292,19 @@ return (
         ref={(node) => (Tinder = node)}
         className="tinder"
       >
-        <MovieCard
+       {isLoaded ?  
+       
+       <MovieCard
             title={details[0].title}
             release={details[0].release}
             score={details[0].vote_average}
-            genre={[{ name: "Genre" }]}
+            genre={details[0].genres ? details[0].genres : details[0].genre}
             duration={details[0].runtime}
             image={details[0].poster_path}
             description={details[0].overview}
             add={true}
             react={false}
-          />
+          /> : <>Loading </>}
        
         
         
